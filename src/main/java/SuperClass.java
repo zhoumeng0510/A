65cd13749c003bcc47a7d4bde7c9710cff346e69
zhoumeng0510 <1058166619@qq.com>
2017-04-05 11:24:16 +0800

    Initial commit

---
+++ src/main/java/SuperClass.java
@@ -0,0 +1,12 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by zhoumeng on
+ * 2017/4/5.
+ * 11:03.
+ */
+public class SuperClass {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
