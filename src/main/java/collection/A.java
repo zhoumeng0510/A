d9b0921a54d3d25f848d9d76d7a16e157ef01446
zhoumeng0510 <1058166619@qq.com>
2017-04-12 10:35:41 +0800

    update

---
+++ src/main/java/collection/A.java
@@ -0,0 +1,12 @@
+package collection;
+
+/**
+ * Created by zhoumeng on
+ * 2017/4/12.
+ * 10:35.
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("--------------------");
+    }
+}
