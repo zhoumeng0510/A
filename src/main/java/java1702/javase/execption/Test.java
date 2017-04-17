de89a198f51e3653ad3662862e883fede04e881d
zhoumeng0510 <1058166619@qq.com>
2017-04-17 09:29:16 +0800

    update

---
+++ src/main/java/java1702/javase/execption/Test.java
@@ -0,0 +1,18 @@
+package java1702.javase.execption;
+
+/**
+ * Created by zhoumeng on
+ * 2017/4/17.
+ * 9:08.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test...");
+        System.out.println("hello".charAt(1));
+        System.out.println(new int[]{1,2,3}[0]);
+        System.out.println(1/1);
+        System.out.println(Integer.valueOf("123"));
+        String s = "hi";
+        System.out.println(s.toUpperCase());
+    }
+}
