d1658c83a4db616dc8701246c083ab590aa1146b
zhoumeng0510 <1058166619@qq.com>
2017-05-05 14:38:17 +0800

    update

--- src/main/java/java1702/javase/reflect/demo/c/Service.java
+++ src/main/java/java1702/javase/reflect/demo/c/Service.java
@@ -1,5 +1,12 @@
 package java1702.javase.reflect.demo.c;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
 /**
  * Created by zhoumeng on
  * 2017/5/5.
@@ -7,5 +14,18 @@ package java1702.javase.reflect.demo.c;
  */
 public class Service {
     public void write() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("src/main/java/java1702/javase/reflect/demo/c/config.properties"));
+            String className = properties.getProperty("className");
+            String methodName = properties.getProperty("methodName");
+
+            Class clazz = Class.forName(className);
+            Method method = clazz.getDeclaredMethod(methodName);
+            Constructor constructor = clazz.getDeclaredConstructor();
+            method.invoke(constructor.newInstance());
+        } catch (IOException | ClassNotFoundException | InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 }
